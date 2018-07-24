8919bf5196e75f4f321ba528f1d9aa2d26bd1227
Ireneusz Norwa <ireneusz.norwa@atendesoftware.pl>
2018-07-24 17:28:16 +0200

    start

---
+++ src/main/java/pl/inorwa/training9/App.java
@@ -0,0 +1,13 @@
+package pl.inorwa.training9;
+
+/**
+ * <DESCRIPTION>
+ * <tr>
+ * <b>Created by:</b> ireneusz.norwa@atendesoftware.pl <br>
+ * <b>Date:</b> 24.07.18 17:25
+ */
+public class App {
+	public static void main(String[] args){
+		System.out.println("START");
+	}
+}
